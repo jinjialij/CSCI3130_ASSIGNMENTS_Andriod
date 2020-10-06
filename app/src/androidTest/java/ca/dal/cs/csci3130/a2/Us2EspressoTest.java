244d868672434a4a870153a6cd141d03d12ebea2
JL JIN <jialijin0@gmail.com>
2020-10-06 13:00:07 -0300

    refactor: rename the test file name for espressoTest

--- app/src/androidTest/java/ca/dal/cs/csci3130/a2/Us2EspressoTest.java
+++ app/src/androidTest/java/ca/dal/cs/csci3130/a2/Us2EspressoTest.java
@@ -12,7 +12,7 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-public class Us2AT3Test {
+public class Us2EspressoTest {
 
     @Rule
     public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<MainActivity>(MainActivity.class);

ba438b09a003a868ed7b2db316364c71c5b5fe89
jlJJ <jl548339@dal.ca>
2020-11-14 21:30:29 -0400

    q2: use factory method design pattern to complete code and pass test

--- app/src/main/java/ca/dal/cs/csci3130/a4/q2/Fruit.java
+++ app/src/main/java/ca/dal/cs/csci3130/a4/q2/Fruit.java
@@ -4,4 +4,34 @@ public interface Fruit {
     //This fruit interface must be implemented with different fruits!
     public String getTaste();
     public String getColor();
+
+    class Mango implements Fruit{
+        public String getTaste(){
+            return "sweet";
+        }
+
+        public String getColor(){
+            return "green";
+        }
+    }
+
+    class Orange implements Fruit{
+        public String getTaste(){
+            return "sour";
+        }
+
+        public String getColor(){
+            return "yellow";
+        }
+    }
+
+    class Grape implements Fruit{
+        public String getTaste(){
+            return "sweet";
+        }
+
+        public String getColor(){
+            return "green";
+        }
+    }
 }
--- app/src/main/java/ca/dal/cs/csci3130/a4/q2/FruitFactory.java
+++ app/src/main/java/ca/dal/cs/csci3130/a4/q2/FruitFactory.java
@@ -4,6 +4,19 @@ public class FruitFactory {
 
     public Fruit getFruit(String fruitName) {
         //your code goes here, this method needs modification!
+        if (fruitName == null){
+            return null;
+        }
+        else if (fruitName.equals("MANGO")){
+            return new Fruit.Mango();
+        }
+        else if (fruitName.equals("ORANGE")){
+            return new Fruit.Orange();
+        }
+        else if (fruitName.equals("GRAPE")){
+            return new Fruit.Grape();
+        }
+
         return null;
     }
 }
